b61ebb9710c4577d53817e6730cc555e52d25d46
Debayan Mondal <81500329+Debayanmondal@users.noreply.github.com>
2023-05-08 14:48:35 +0530

    Update Login.java

--- bank_mangement/src/bank/management/system/Login.java
+++ bank_mangement/src/bank/management/system/Login.java
@@ -18,7 +18,7 @@ public class Login extends JFrame implements ActionListener{
     Login(){
         setTitle("AUTOMATED TELLER MACHINE");
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("C:\Users\debay\vs_code_for_java\bank mangement system\bank_mangement\src\icons\logo.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l11 = new JLabel(i3);
